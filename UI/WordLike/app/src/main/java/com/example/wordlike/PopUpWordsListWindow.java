da33640feabbcdb492bff314b87fa03a787a883f
poetree77 <48195130+poetree77@users.noreply.github.com>
2019-05-24 21:01:50 +0900

    ui틀

---
+++ UI/WordLike/app/src/main/java/com/example/wordlike/PopUpWordsListWindow.java
@@ -0,0 +1,13 @@
+package com.example.wordlike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopUpWordsListWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_words_list_window);
+    }
+}
---
+++ UI/WordLike/app/src/main/java/com/example/wordlike/SignWindow.java
@@ -0,0 +1,14 @@
+package com.example.wordlike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_window);
+    }
+
+}
