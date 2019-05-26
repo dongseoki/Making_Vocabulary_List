eb91481ec799ec2b781cee48d2876b144b80c896
dongseoki <43495319+dongseoki@users.noreply.github.com>
2019-05-26 22:39:14 +0900

    수정 중-Recycle View 에서 막힘

--- WordLikeProject/app/src/main/java/com/example/wordlikeproject/MainWindow.java
+++ WordLikeProject/app/src/main/java/com/example/wordlikeproject/MainWindow.java
@@ -1,13 +1,49 @@
 package com.example.wordlikeproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainWindow extends AppCompatActivity {
-
+    EditText searchBar;
+    String ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
+        Intent intent  = getIntent();
+        processIntent(intent);
+    }
+    public void processIntent(Intent intent){
+        if(intent !=null){
+            ID = intent.getStringExtra("ID");
+        }
+    }
+    public void searchButtonOnClicked(View view){
+        searchBar = (EditText) findViewById(R.id.searchBar);
+        String searchString = searchBar.getText().toString();
+        if(!searchString.equals("")) {
+            Intent intent = new Intent(this, SearchingResultWindow.class);
+            intent.putExtra("ID", ID);
+            intent.putExtra("searchString", searchString);
+            startActivityForResult(intent, 111);
+        }
+    }
+
+    public void notificationButtonOnClicked(View view){
+        Intent intent = new Intent(this, NotificationWindow.class);
+        startActivityForResult(intent, 111);
+    }
+
+    public void memberSecessionButtonOnClicked(View view){
+        Toast.makeText(getApplicationContext(), "회원탈퇴 버튼이 눌림", Toast.LENGTH_SHORT).show();
+    }
+
+    public void wordbookListButtonOnClicked(View view){
+        Intent intent = new Intent(this, WordBookListWindow.class);
+        startActivityForResult(intent, 111);
     }
 }
--- WordLikeProject/app/src/main/java/com/example/wordlikeproject/SignWindow.java
+++ WordLikeProject/app/src/main/java/com/example/wordlikeproject/SignWindow.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class SignWindow extends AppCompatActivity {
     boolean loginState;
@@ -14,17 +19,27 @@ public class SignWindow extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_window);
     }
-    public void signInButtonOnClicked(View view){
+
+    public void signInButtonOnClicked(View view) {
         ID = "fail";
         ID = signWithGoogle();
-        if(!ID.equals("fail")) {
+        if (!ID.equals("fail")) {
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(getFilesDir() + "ID.text", false));
+                bw.write(ID);
+                bw.close();
 
-            Intent intent = new Intent(this, MainWindow.class);
-            intent.putExtra("ID", ID);
-            startActivityForResult(intent, 111);
+                Intent intent = new Intent(this, MainWindow.class);
+                intent.putExtra("ID", ID);
+                startActivityForResult(intent, 111);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
-    public String signWithGoogle(){
+
+    public String signWithGoogle() {
         // API 를 이용한 로그인 시도 과정.
         // 지금은 테스트로 어쩔수 없이 id로 leedongseok 을 반환
         // if(실패..?)
