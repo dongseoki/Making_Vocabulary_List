45f934d7cd3cfc09c2e3e7b31852c915c53ce62b
ckdgus0505 <ckdgus0505@gmail.com>
2019-05-29 22:16:33 +0900

    다운로드센터 목록 받아오기

---
+++ UI/WordLike2/app/src/main/java/com/example/wordlike/RecyclerAdapter.java
@@ -0,0 +1,69 @@
+package com.example.wordlike;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.wordlike.Data;
+import com.example.wordlike.R;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+
+    // adapter에 들어갈 list 입니다.
+    private ArrayList<Data> listData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // LayoutInflater를 이용하여 전 단계에서 만들었던 item.xml을 inflate 시킵니다.
+        // return 인자는 ViewHolder 입니다.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        // Item을 하나, 하나 보여주는(bind 되는) 함수입니다.
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // RecyclerView의 총 개수 입니다.
+        return listData.size();
+    }
+
+    void addItem(Data data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+
+    // RecyclerView의 핵심인 ViewHolder 입니다.
+    // 여기서 subView를 setting 해줍니다.
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView1;
+        private TextView textView2;
+        private ImageView imageView;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+
+        void onBind(Data data) {
+            textView1.setText(data.getTitle());
+            textView2.setText(data.getAuth());
+            imageView.setImageResource(data.getResId());
+        }
+    }
+}
--- UI/WordLike2/app/src/main/java/com/example/wordlike/WordListsDownload.java
+++ UI/WordLike2/app/src/main/java/com/example/wordlike/WordListsDownload.java
@@ -1,13 +1,122 @@
 package com.example.wordlike;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WordListsDownload extends AppCompatActivity {
+
+    private RecyclerAdapter adapter;
+    String urll = "http://ec2-13-209-19-111.ap-northeast-2.compute.amazonaws.com/select_WordsList.php";
+    TextView searchEdit;
+    private static String TAG = "phptest";
+    String mJsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words_lists_download_center);
+        searchEdit = (TextView) findViewById(R.id.searchEdit);
+        init();
+
+        PhpTest task = new PhpTest();
+        task.execute(urll);
+
+    }
+
+    private void init() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void getResult(String json) {
+
+        String TAG_JSON = "Wordlists";
+        String TAG_ID = "membershipNumber";
+        String TAG_TITLE = "Title";
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            String wordarrayDB = jsonObject.getString("WordlistDB");
+            JSONArray jsonArray = new JSONArray(wordarrayDB);
+            for (int i=0; i < jsonArray.length(); i++) {
+                JSONObject subJsonObject = jsonArray.getJSONObject(i);
+                String num = subJsonObject.getString("membershipNumber");
+                String title = subJsonObject.getString("Title");
+
+                Data data = new Data();
+                data.setTitle(title);
+                data.setAuth(num);
+                adapter.addItem(data);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private class PhpTest extends AsyncTask<String,String,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            String output = "";
+            try {
+                //연결 url 설정
+                URL url = new URL(params[0]);
+
+                //커넥션 객체 생성
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                //연결되었으면
+                if(conn != null){
+                    conn.setConnectTimeout(10000);
+                    conn.setUseCaches(false);
+                    //연결된 코드가 리턴되면
+                    if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                        int i = 0 ;
+                        for(;;){
+                            //웹상에 보이는 텍스트를 라인단위로 읽어 저장
+                            String line = br.readLine();
+                            if(line == null)
+                                break;
+                            i++;
+                            output += line;
+                        }
+                        getResult(output);
+                        br.close();
+                    }
+                    conn.disconnect();
+                }else{
+                    System.out.println("실패ㅡㅡ");
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return output;
+        }
     }
 }
+
+
+
